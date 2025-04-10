b4595448704911175a5e185e5a7564d179c1b131
Dulanji <amandasathsarani03@gmail.com>
2025-04-10 09:51:53 +0530

    basic

---
+++ src/main/java/com/assignment/tictactoe/service/chatapplication/Controller/ClientViewController.java
@@ -0,0 +1,30 @@
+package com.assignment.tictactoe.service.chatapplication.Controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientView {
+
+    @FXML
+    private AnchorPane ap1;
+
+    @FXML
+    private Button sendbtn;
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    void sendbtnOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/assignment/tictactoe/service/chatapplication/Controller/ServerViewController.java
@@ -0,0 +1,29 @@
+package com.assignment.tictactoe.service.chatapplication.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ServerView {
+
+    @FXML
+    private AnchorPane ap2;
+
+    @FXML
+    private Button serversendbtn;
+
+    @FXML
+    private TextField servertextfield;
+
+    @FXML
+    private TextArea servertxtArea;
+
+    @FXML
+    void sendbtnOnAction(ActionEvent event) {
+
+    }
+
+}
